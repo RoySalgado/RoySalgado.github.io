03748df3552c3ffb2027651d9d38232eb38f0998
Roy Salgado <roy21rasm@gmail.com>
2020-07-11 17:01:19 -0500

    Correccion en la barra superior

--- js/activeS.js
+++ js/activeS.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
     });
     //detectar ancho y poner el valor en height
     var dn_width = $('.diamante').width();
-    
+    //Clase para el scroll en la barra de notificaciones
+    var slider=$("#slider");
+    $(window).scroll(function(){
+        var scroll=$(window).scrollTop();
+        if(scroll >= $("#nosotros").offset().top-200){
+            $("#nav").addClass("nav-overlay");
+        }else{
+            $("#nav").removeClass("nav-overlay");
+        }
+    });
+    $(window).scroll();
 });
